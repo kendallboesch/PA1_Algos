17b6e6ab06a454e9b327592238753f2e8cbf7b81
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2022-09-21 20:22:40 -0500

    Fixed sort code 3 errors but still having counter incrementing issues

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <cmath>
-#include <random>
 #include "shellsort.cpp"
 
 using namespace std;
@@ -10,34 +9,53 @@ using namespace std;
 int main() {
     cout << " Code" << '\t' << "trial#" << '\t' << "n" << '\t' << "num comp" << endl;
 
-    int sort, trial, n, comp;
-    sort = 0;
-    trial = 0;
-    n = 500;
+    int sort, trial, n, total_trials;
+
     vector<C1> k;
-    int s,j,i;
 
-    for (int s = 0; s < 3; s++) {
-        for (int j = 0; j < 10; j++) {
+    vector<int> num;
+    num.push_back(500);
+    num.push_back(1000);
+    num.push_back(1500);
+//    num.push_back(2000);
+//    num.push_back(2500);
+//    num.push_back(3000);
+//    num.push_back(3500);
+//    num.push_back(4000);
+//    num.push_back(4500);
+//    num.push_back(5000);
+    total_trials = 1000;
 
 
-            for (int i = 0; i < 100; i++) {
-                int a, b, c;
-                a = rand() % 100;
-                b = rand() % 100;
-                c = rand() % 100;
+    for ( sort = 0; sort < 1; sort++) {       // Sort loop
 
-                C1 x(a, b, c);
-                k.push_back(x);
+            for(int y = 0; y < 1; y++) {
+                for (trial = 0; trial < total_trials; trial++) {  // trial num loop
+
+                    for (n = 0;n < num[y]; n++) {     // num
+                        int a, b, c;
+                        a = rand() % 100;
+                        b = rand() % 100;
+                        c = rand() % 100;
+
+                        C1 x(a, b, c);
+                        k.push_back(x);
+                        x.compareCount = 0;
+                    }
+                    //cout << endl << C1::compareCount << '\t';
+
+                    C1::compareCount = 0;
+                    //cout << C1::compareCount << '\t' << endl ;
+                    shellSort(k, 0);
+                    cout << sort << '\t' << '\t' << trial << '\t' << '\t' << num[y] << '\t' << '\t' << C1::compareCount << endl;
+
+                }
+
             }
-            C1::compareCount = 0;
-            shellSort(k, 0);
-            cout << s << '\t' << '\t' << j << '\t' << '\t' << 100 << '\t' << '\t' << C1::compareCount << endl;
-        }
+
+        cout << "------------------------------------------" << endl;
 
     }
-
-
 
 
 
--- shellsort.cpp
+++ shellsort.cpp
@@ -3,37 +3,31 @@
 #include <cmath>
 
 using namespace std;
-int fact(int);
-int comb(int);
+double fact(double );
+double  comb(double );
 void shellSort(vector<C1>&, int);
 
-void shellSort(vector<C1>& varray, int code)
-{
+void shellSort(vector<C1>& varray, int code) {
     vector<int> hlist;
     //  int n = varray.size();
-    double base, k, prevK, result;
+    unsigned int base, k, prevK, result;
     int n = varray.size();
-    if(code == 0)
-    {
+    if (code == 0) {
         hlist.push_back(1);
-    }
-    else if (code == 1)
-    {
-      //  cout << "CODE: 01" << endl;
+    } else if (code == 1) {
+        //  cout << "CODE: 01" << endl;
         base = 2;
-        k  = 0;
+        k = 0;
 
         bool calculatingKVal = true;
 
-        while(calculatingKVal)
-        {
+        while (calculatingKVal) {
             prevK = k;
             k++;
 
             result = pow(base, k);
-         //   cout << "K: " << k << endl << "result: " << result << endl;
-            if(result >= n)
-            {
+            //   cout << "K: " << k << endl << "result: " << result << endl;
+            if (result >= n) {
                 k = prevK;
 //                cout << "Final K Val: " << k << endl;
 //                cout << "Final Result Val : " << pow(base, k) << endl;
@@ -43,35 +37,30 @@ void shellSort(vector<C1>& varray, int code)
         }
 
         //POPULATE HLIST
-        double term = pow(base, k);
+        unsigned int term = pow(base, k);
 
-        double kInc = 0;
-        while(term > 1)
-        {
+        int kInc = 0;
+        while (term > 1) {
             //cout << "TERM = 2 ^ (" << k << '-' << kInc << ')' << endl;
             term = pow(base, k - kInc);
             //cout << term << endl;
             hlist.push_back(term);
             kInc++;
         }
-    }
-    else if(code == 2)
-    {
-       // cout << "CODE: 02" << endl;
+    } else if (code == 2) {
+        // cout << "CODE: 02" << endl;
         base = 2;
         k = prevK = 0;
 
         bool calculatingKVal = true;
 
-        while(calculatingKVal)
-        {
+        while (calculatingKVal) {
             prevK = k;
             k++;
 
             result = pow(base, k) - 1;
             //cout << "K: " << k << endl << "result: " << result << endl;
-            if(result >= n)
-            {
+            if (result >= n) {
                 k = prevK;
 //                cout << "Final K Val: " << k << endl;
 //                cout << "Final Result Val : " << pow(base, k) - 1 << endl;
@@ -80,47 +69,42 @@ void shellSort(vector<C1>& varray, int code)
         }
 
         //POPULATE HLIST
-        double term = pow(base, k) - 1;
+        unsigned int term = pow(base, k) - 1;
 
         //hlist.push_back(term);
-        double kInc = 0;
-        while(term > 1)
-        {
-           // cout << "TERM = 2 ^ (" << k << '-' << kInc << ')' << "-1" << endl;
+        unsigned int kInc = 0;
+        while (term > 1) {
+            // cout << "TERM = 2 ^ (" << k << '-' << kInc << ')' << "-1" << endl;
             term = pow(base, k - kInc) - 1;
             //cout << term << endl;
             hlist.push_back(term);
             kInc++;
         }
-    }
-    else if (code == 3)
-    {
+    } else if (code == 3) {
         k = 2;
         //cout << "CODE: 03" << endl;
         result = 0;
         bool calculatingKVal = true;
-        while(calculatingKVal)
-        {
+        while (calculatingKVal) {
             prevK = k;
             k++;
-            //cout << "K: " << k << endl << "Result: " << result << endl;
             result = comb(k);
-            if(result >= n)
-            {
+            cout << "K: " << k << endl << "Result: " << result << endl;
+            if (result >= n) {
                 k = prevK;
-                //cout << "Final K: " << k << endl;
+                cout << "Final K: " << k << endl;
                 result = comb(k);
-                //cout << "Final result val: " << result << endl;
+                cout << "Final result val: " << result << endl;
                 calculatingKVal = false;
             }
         }
         // POPULATE HLIST
 
-        int term = comb(k);
-       // hlist.push_back(term);
+        // int term = comb(k);
+        unsigned int term = result;
+        // hlist.push_back(term);
         int kDec = 0;
-        while( term >= 1)
-        {
+        while (term >= 1) {
             term = comb(k - kDec);
             hlist.push_back(term);
             kDec++;
@@ -137,34 +121,31 @@ void shellSort(vector<C1>& varray, int code)
 
     int count = 0;
     C1::compareCount = 0;
-    for(int l = 0; l < hlist.size(); l++)
-    {
+    for (int l = 0; l < hlist.size(); l++) {
         h = hlist[l];
-        for(int j = 1; j < n; j++)
-        {
+        for (int j = 1; j < n; j++) {
             C1 key(varray[j]);
             int i = j - h;
-            while(i >= 0 && key < varray[i])
-            {
+            while (i >= 0 && key < varray[i]) {
 
-                C1::compareCount++;
+               // C1::compareCount++;
                 count++;
-              //  cout << "C: " << count << endl;
+                //  cout << "C: " << count << endl;
 
                 varray[i + h] == varray[i];
                 i = i - h;
             }
-            varray[i+h] == key;
+            varray[i + h] == key;
         }
     }
 
-    cout << "COUNT: " << count << endl;
+    // cout << "COUNT: " << count << endl;
     C1::compareCount = count;
 
 }
-int fact(int n)
+double  fact(double n)
 {
-    if(n == 1 || n == 0)
+    if(n <= 1)
     {
         return 1;
     }
@@ -173,7 +154,7 @@ int fact(int n)
         return(n * fact(n-1));
     }
 }
-int comb(int k)
+ double comb(double k)
 {
 
     return fact(k) / (fact(2) * fact(k - 2));
